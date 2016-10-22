0833a6a75c57a85ddec31c317594dada245c1da2
bharding2 <b.harding@gmail.com>
2016-10-22 12:25:40 -0700

    authenticat init for server closes #7

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ if (!process.env.APP_SECRET) throw new Error('you need an APP_SECRET env variabl
 
 const express = require('express');
 const mongoose = require('mongoose');
+const Authenticat = require('authenticat');
 
 const app = express();
 const PORT = process.env.PORT || 5555;
-// authenticat?
 
 // require routers
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/slothbear_ht_DB');
+const connection = mongoose.createConnection(process.env.MONGODB_URI || 'mongodb://localhost/slothbear_ht_DB');
+const authenticat = new Authenticat(connection);
 
 // router mounting
+app.use('/api', authenticat.router);
 
 app.use(express.static('./build'));
 
