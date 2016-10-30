a55ceb898829374575f098e991738ba5c57b88d4
bharding2 <b.harding@gmail.com>
2016-10-30 11:37:07 -0700

    refactor

--- routes/ht_auth_router.js
+++ routes/ht_auth_router.js
@@ -8,27 +8,27 @@ module.exports = function(connection, authenticat) {
   // TODO: error handling on all the things
 
   const User = require(__dirname + '/../models/user')(connection);
-  
+
   var htAuthRouter = Router();
 
   htAuthRouter.get('/trigger_ht_auth', (req, res) => {
     var newUser = new User();
 
     newUser.save((err, user) => {
-      var oauth = {
+      var requestTokenUrl = 'https://chpp.hattrick.org/oauth/request_token.ashx';
+      var requestTokenOAuth = {
         callback: 'http://127.0.0.1:5555/auth/ht_auth/' + user._id,
         consumer_key: process.env.HT_CONSUMER_KEY,
         consumer_secret: process.env.HT_CONSUMER_SECRET
       };
-      var url = 'https://chpp.hattrick.org/oauth/request_token.ashx';
 
-      request.get({url: url, oauth: oauth}, function (err, response, body) {
-        var req_data = qs.parse(body);
-        var uri = 'https://chpp.hattrick.org/oauth/authorize.aspx' + '?' + qs.stringify({oauth_token: req_data.oauth_token});
+      request.get({ url: requestTokenUrl, oauth: requestTokenOAuth }, (err, response, body) => {
+        var requestTokenData = qs.parse(body);
+        var authorizeUri = 'https://chpp.hattrick.org/oauth/authorize.aspx' + '?' + qs.stringify({ oauth_token: requestTokenData.oauth_token });
 
-        user.htTokenSecret = req_data.oauth_token_secret;
+        user.htTokenSecret = requestTokenData.oauth_token_secret;
         user.save((err, user) => {
-          res.redirect(uri);
+          res.redirect(authorizeUri);
         });
       });
     });
@@ -36,28 +36,30 @@ module.exports = function(connection, authenticat) {
 
   htAuthRouter.get('/ht_auth/:userID', (req, res) => {
     User.findById(req.params.userID, (err, user) => {
-      var oauth = {
+      var accessTokenUrl = 'https://chpp.hattrick.org/oauth/access_token.ashx';
+      var accessTokenOAuth = {
         consumer_key: process.env.HT_CONSUMER_KEY,
         consumer_secret: process.env.HT_CONSUMER_SECRET,
         token: req.query.oauth_token,
         token_secret: user.htTokenSecret,
         verifier: req.query.oauth_verifier,
       };
-      var url = 'https://chpp.hattrick.org/oauth/access_token.ashx';
-      request.get({ url: url, oauth: oauth }, function (err, response, body) {
-        var access_data = qs.parse(body);
+
+      request.get({ url: accessTokenUrl, oauth: accessTokenOAuth }, (err, response, body) => {
+        var accessTokenData = qs.parse(body);
 
         // TODO: save access token to user
 
         var teamUrl = 'http://chpp.hattrick.org/chppxml.ashx?file=teamdetails&version=3.2';
-        var oauth = {
+        var teamOAuth = {
           consumer_key: process.env.HT_CONSUMER_KEY,
           consumer_secret: process.env.HT_CONSUMER_SECRET,
-          token: access_data.oauth_token,
-          token_secret: access_data.oauth_token_secret,
+          token: accessTokenData.oauth_token,
+          token_secret: accessTokenData.oauth_token_secret,
         };
-        request.get({ url: teamUrl, oauth: oauth }, function (err, response, body) {
-          parseString(body, { explicitArray: false }, function (err, result) {
+
+        request.get({ url: teamUrl, oauth: teamOAuth }, (err, response, body) => {
+          parseString(body, { explicitArray: false }, (err, result) => {
             // TODO: save team data
             // TODO: replace placeholder redirect
             res.redirect('/?token=success');
